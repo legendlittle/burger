d01cd968c099da7faae1287d7163d905bbbfa0f3
Davey Wong <notwdjw@gmail.com>
2020-01-23 15:18:11 -0800

    Set up Directory folders and files and added npm packages; created sql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('Umami', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Slam', false);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Monster', false);
