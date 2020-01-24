0341f50a5dc7ecd83a49c7d5c8571260031b1b70
Davey Wong <notwdjw@gmail.com>
2020-01-24 12:28:33 -0800

    Created SQL queries in orm.js and exported the module

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,30 @@
 var connection = require("./connection.js");
 
+var orm = {
+    selectAll: function(selectFrom){
+        var query = `SELECT * FROM ??`;
+        connection.query(query, [selectFrom], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        })
+    },
+
+    insertOne: function(db, table, argOne, argTwo, valueOne, valueTwo){
+        var query = `USE ?? INSERT INTO ??(?, ?) VALUES(?, ?)`;
+        connection.query(query, [db, table, argOne, argTwo, valueOne, valueTwo], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        })
+
+    },
+    
+    updateOne: function(db, table, propOne, valOne, propTwo, valTwo, burgerID){
+        var query = `USE ?? UPDATE ?? SET ?? = '??', ??= '??' WHERE id = ??`;
+        connection.query(query, [db, table, propOne, valOne, propTwo, valTwo, burgerID], function(err, result){
+            if (err) throw err;
+            console.log(result);
+        })
+    }
+};
+
+module.exports = orm;
